a21f57860330fc04c66e639d49fd6b007816eea9
Jack Nevitt <jack.nevitt@made.com>
2020-02-07 13:12:45 +0000

    added apollo client side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,53 @@
 import { NextPage } from 'next';
 import Layout from '../components/Layout/Layout';
 
+import ApolloClient, { gql } from 'apollo-boost';
+import fetch from 'isomorphic-unfetch';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'https://preprod-elastigraph.made.com/graphql',
+  fetch,
+});
+
+const QUERY = gql`
+  {
+    router(store: GB, url: "sofas-and-armchairs/sofas") {
+      views {
+        __typename
+        ... on TagView {
+          name
+        }
+      }
+    }
+  }
+`;
+
 type Props = {
   userAgent: string;
 };
 
-const Home: NextPage<Props> = ({ userAgent }: Props) => (
-  <Layout>
-    <h1>Home Page</h1>
-    <p>Work in progress</p>
-    <p>{userAgent}</p>
-  </Layout>
-);
+const TestApollo: React.FC = () => {
+  const { loading, error, data } = useQuery(QUERY);
+  if (loading) return <pre>{'Loading…'}</pre>;
+  if (error) return <pre>{error.message}</pre>;
+  return <pre>{JSON.stringify(data)}</pre>;
+};
+
+const Home: NextPage<Props> = ({ userAgent }: Props) => {
+  return (
+    <ApolloProvider client={client}>
+      <Layout>
+        <h1>Home Page</h1>
+        <p>Work in progress</p>
+        <p>{userAgent}</p>
+        <TestApollo />
+      </Layout>
+    </ApolloProvider>
+  );
+};
 
 Home.getInitialProps = async ({ req }): Promise<Props> => {
   const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
