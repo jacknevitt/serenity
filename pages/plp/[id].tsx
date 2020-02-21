cb8b9a8ab4638b945d53d1f4b7e9a44aad8249da
Jack Nevitt <jack.nevitt@made.com>
2020-02-21 12:22:01 +0000

    package.json changes and layout included in other page

--- pages/plp/[id].tsx
+++ pages/plp/[id].tsx
@@ -1,15 +1,16 @@
 import { NextPage } from 'next';
 import { Button } from 'atoms';
+import Layout from '../../components/Layout/Layout';
 
 type Props = {
   id: string | string[];
 };
 
 const ProductListingPage: NextPage<Props> = ({ id }: Props) => (
-  <>
+  <Layout>
     <h1>{id}</h1>
     <Button>Click</Button>
-  </>
+  </Layout>
 );
 
 ProductListingPage.getInitialProps = ({ query }): Props => {
